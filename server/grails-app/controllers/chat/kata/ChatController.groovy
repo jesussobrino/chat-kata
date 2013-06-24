3e7b8b1f9780410ef12b9fb1132b0f5af5846a95
Jesús <jesus.sobrino@bqreaders.com>
2013-06-24 11:03:56 +0200

    primera subida buena (2)

--- server/grails-app/controllers/chat/kata/ChatController.groovy
+++ server/grails-app/controllers/chat/kata/ChatController.groovy
@@ -1,12 +1,45 @@
 package chat.kata
 
-class ChatController {
+//import chat.kata.command.SendChatMessageCommand;
 
+class ChatController {
+	ChatService chatService
+
+	
+	/**
+	 * Método listar (SEND)
+	 * Montará un json, al que le añadirá:	- messages(nick + message)
+	 * 										- last_seq (secuencia)
+	 * @param seq
+	 * @return
+	 */
 	def list(Integer seq) {
-		//TODO: implement me
+		if(hasErrors()){
+			log.error("Invalid seq: ${errors.getFieldError('seq').rejectedValue}")
+			//TODO: implement me
+		}
+		final Collection<ChatMessage> collector = []
+		final int lastSequence = chatService.collectChatMessages(collector, seq)
+
+		render(contentType: "text/json"){
+			messages = []
+			for (int i = 0; i < collector.size(); i++) {
+				ChatMessage chat = collector[i]
+				messages.add(nick:chat.nick, message:chat.message)
+			}
+			last_seq = lastSequence
+		}
 	}
 
+
+	/**
+	 * Método enviar (POST)
+	 * @return
+	 */
 	def send(){
-		//TODO: implement me
+		def msgs = new ChatMessage(request.JSON)
+		chatService.putChatMessage(msgs);
+		
+		render(status: 201)
 	}
 }
--- server/grails-app/services/chat/kata/ChatService.groovy
+++ server/grails-app/services/chat/kata/ChatService.groovy
@@ -1,8 +1,18 @@
 package chat.kata
 
+import java.util.concurrent.locks.Lock
+import java.util.concurrent.locks.ReentrantReadWriteLock
+
+
 
 class ChatService {
-	
+
+	private ArrayList <ChatMessage> listaMensajes = new ArrayList<>()
+	private final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+	private final Lock r = rwl.readLock();
+	private final Lock w = rwl.writeLock();
+
+
 	/**
 	 * Collects chat messages in the provided collection
 	 * 
@@ -12,17 +22,45 @@ class ChatService {
 	 * @return the sequence of the last message collected.
 	 */
 	Integer collectChatMessages(Collection<ChatMessage> collector, Integer fromSeq = null){
-		//TODO: implement me
+		r.lock()
+		try{
+			if(fromSeq == null){
+				fromSeq = 0
+			}else{
+				fromSeq = fromSeq+1
+			}
+
+			while(listaMensajes.size() > fromSeq){
+				collector.add(listaMensajes[fromSeq])
+				fromSeq++
+			}
+
+			fromSeq--
+		}
+		
+		finally{
+			r.unlock()
+		}
+		
+		return fromSeq
 	}
-	
+
+
 	/**
 	 * Puts a new message at the bottom of the chat
 	 * 
 	 * @param message the message to add to the chat
 	 */
 	void putChatMessage(ChatMessage message){
-		//TODO: implement me
+		w.lock()
+		
+		try{
+			if(message != null)
+			listaMensajes.add(message)
+		}
+		
+		finally{
+			w.unlock()
+		}
 	}
-	
-	
 }
